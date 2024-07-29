0c83fd70bfb44aa4e3281cce47d52914d8b4c36c
Yasir Khan <yk5645123@gmail.com>
2024-07-29 04:52:19 -0700

    made some changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import CompanyCollaborationpage from './Components/Company-Collaboration-page';
 import Contactus from './Components/Contact-us';
 import Loginform from './Components/Login-form';
 import JobHome from './Components/Job Portal/home';
+import JobCompany from './Components/Job Portal/Job-Company';
+import HiringNow from './Components/Job Portal/Hiring-Now';
+import Aboutus from './Components/Job Portal/About-us';
+import JobContactus from './Components/Job Portal/Job-Contact-us';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -40,15 +44,32 @@ function App() {
       path:"/Contact-us-page",
       element:<><Contactus/></>
           },
-          {
-            path:"/internee-login",
-            element:<><Loginform/></>
-                },
-                {
-                  path:"/Job-Portal",
-                  element:<><JobHome/></>
-                      },
-  ]);
+            {
+              path:"/internee-login",
+              element:<><Loginform/></>
+                  },
+                  {
+                    path:"/Job-Portal",
+                    element:<><JobHome/></>
+                        },
+                        {
+                          path:"/Job-Companies",
+                          element:<><JobCompany/></>
+                              },
+                              {
+                                path:"/Hiring-Now",
+                                element:<><HiringNow/></>
+                                    },
+                                    {
+                                      path:"/About-Us",
+                                      element:<><Aboutus/></>
+                                          },
+                                        {
+                                          path: "/Contact-Us",
+                                          element: <><JobContactus/></>,
+                                        }
+        ]);
+
   return (
     <div className="App">
     
